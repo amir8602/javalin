e1abf409d75b8611681994b6adbbbcac139693c4
amir8602 <amirreza8602@gmail.com>
2021-10-30 14:23:27 +0330

    End Se1

--- src/Java/Se1/Main.java
+++ src/Java/Se1/Main.java
@@ -1,7 +1,21 @@
 package Java.Se1;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
+        Integer data = new Integer(12);
+        data++;
+        System.out.println(data);
+        Scanner scanner = new Scanner(System.in);
+        scanner.close();
+
+
+        // FirstData ---> pascal case ---> Class name
+        // firstData ---> camel case --->method Name
+        // first-data ---> kebab case
+        // first_name ---> snake case
+        // operator overloading nadarim
     }
 }
