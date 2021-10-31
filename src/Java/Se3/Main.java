fde3e12a8a9f74e153c723a64a082d1e985153ae
amir8602 <amirreza8602@gmail.com>
2021-10-31 17:09:51 +0330

    Start Se3

---
+++ src/Java/Se3/Main.java
@@ -0,0 +1,7 @@
+package Java.Se3;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Java/Se3/Person.java
@@ -0,0 +1,47 @@
+package Java.Se3;
+
+public class Person {
+
+    private String name;
+    private String family;
+    private Integer age;
+
+    public Person(String name, String family, Integer age) {
+        this.name = name;
+        this.family = family;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public void setFamily(String family) {
+        this.family = family;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", family='" + family + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
